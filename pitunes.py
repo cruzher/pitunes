e3686fa999d47a4e2b86cb21d072727784c15ea3
cruzher <kevin@kevins-mbp.lan>
2013-07-03 20:58:46 +0200

    .

--- pitunes.py
+++ pitunes.py
@@ -2,6 +2,6 @@ import string
 import subprocess
 import time
 from datetime import datetime
-from Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
+from Adafruit_CharLCDPlate/Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
 
 
