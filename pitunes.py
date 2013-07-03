0b3accf00a5ea9d62b8e12457bb5ae8ce5852493
cruzher <kevin@kevins-mbp.lan>
2013-07-03 21:52:09 +0200

    .

--- pitunes.py
+++ pitunes.py
@@ -1,6 +1,6 @@
 import string
 import subprocess
-import time
+from time import sleep
 from datetime import datetime
 from Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
 import gaugette.rotary_encoder
