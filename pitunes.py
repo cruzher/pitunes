ec1d46fbb8d9118a1f64e5aaa41a743886a4b5ae
cruzher <kevin@kevins-mbp.lan>
2013-07-03 22:28:00 +0200

    .

--- pitunes.py
+++ pitunes.py
@@ -57,7 +57,7 @@ while True:
 	
 	if (mop_track != mop_track_last):
 		lcd.setCursor(0,1)
-		lcd.message(mop_track)
+		lcd.message(mop_track[:20])
 		print(mop_track[:20])
 		mop_track_last = mop_track
 	
