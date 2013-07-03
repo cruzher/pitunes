d88ebbb7cfad790ab697d9dd53be99b3634a787a
cruzher <kevin@kevins-mbp.lan>
2013-07-03 21:44:42 +0200

    .

--- pitunes.py
+++ pitunes.py
@@ -25,9 +25,22 @@ volume_switch = gaugette.switch.Switch(volume_sw)
 vol_sw_last = None
 vol_state_last = None
 
+station_a = 11
+station_b = 10
+station_sw = 6
+
+station_enc = gaugette.rotary_encoder.RotaryEncoder(station_a, station_b)
+station_switch = gaugette.switch.Switch(station_sw)
+
+stn_sw_last = None
+stn_state_last = None
+
+
 while True:
 	vol_state = volume_enc.rotation_state()
 	vol_sw = volume_switch.get_state()
+	stn_state = station_enc.rotation_state()
+	stn_sw = station_switch.get_state()
 	
 	if (vol_state != vol_state_last or vol_sw != vol_sw_last):
 		vol_state_last = vol_state
@@ -35,4 +48,10 @@ while True:
 		
 		if (vol_sw==1):
 			mekk = subprocess.Popen("mpc play", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
+			
+	if (stn_state != stn_state_last or stn_sw != stn_sw_last):
+		stn_state_last = stn_state
+		stn_sw_last = stn_sw
+		
+		if (stn_sw==1):
+			mekk = subprocess.Popen("mpc stop", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
