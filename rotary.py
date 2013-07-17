b409af91b7f93c4c4cd00ea1a6f462ae545a3253
cruzher <kevin@Kevins-MacBook-Pro.local>
2013-07-17 07:51:08 +0200

    .

--- rotary.py
+++ rotary.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python
 
+#Rotary Encoders
+enc_right_pin_a = 11
+enc_right_pin_b = 10
+sw_right_pin = 6
+enc_left_pin_a = 14
+enc_left_pin_b = 13
+sw_left_pin = 12
+enc_right = gaugette.rotary_encoder.RotaryEncoder(enc_right_pin_a, enc_right_pin_b)
+sw_right = gaugette.switch.Switch(sw_right_pin)
+enc_left = gaugette.rotary_encoder.RotaryEncoder(enc_left_pin_a, enc_left_pin_b)
+sw_left = gaugette.switch.Switch(sw_left_pin)
+
 def checkinput():
 	global volume_current
 	global mopidy_playing
