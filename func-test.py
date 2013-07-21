4a92402091bb0d9aca6682d9642f3b3f8da84f89
cruzher <kevin@Kevins-MacBook-Pro.local>
2013-07-21 13:06:15 +0200

    .

--- func-test.py
+++ func-test.py
@@ -23,14 +23,9 @@ lcd.begin(20, 4)
 lcd.clear()
 
 #Rotary Encoders
-enc_right_pin_a = 14
-enc_right_pin_b = 13
-sw_right_pin = 12
-enc_left_pin_a = 11
-enc_left_pin_b = 10
-sw_left_pin = 6
-enc_right = gaugette.rotary_encoder.RotaryEncoder(enc_right_pin_a, enc_right_pin_b)
-sw_right = gaugette.switch.Switch(sw_right_pin)
+enc_left_pin_a = 14
+enc_left_pin_b = 13
+sw_left_pin = 12
 enc_left = gaugette.rotary_encoder.RotaryEncoder(enc_left_pin_a, enc_left_pin_b)
 sw_left = gaugette.switch.Switch(sw_left_pin)
 
@@ -41,7 +36,7 @@ interface_change_track = False
 interface_change_playlist = False
 mopidy_playlist_length = 10
 mopidy_playlist_position = 5
-mopidy_playlist = ["Row 1", "Row 2", "Row 3", "Row 4", "Row 5", "Row 6", "Row 7", "Row 8", "Row 9", "Row 10"]
+mopidy_playlist = ["Row 1", "Row 2"]
 menu_position = 0
 menu_lcd_position = 1
 menu_lcd_start = 0
@@ -71,28 +66,34 @@ def checkinputs():
 	global menu_timeout
 	
 	while True:
-		enc_right_delta = enc_right.get_delta()
-		enc_right_seq = enc_right.rotation_sequence()
-		sw_right_state = sw_right.get_state()
+		enc_left_delta = enc_left.get_delta()
+		enc_left_seq = enc_left.rotation_sequence()
+		sw_left_state = sw_left.get_state()
 		
 		if (interface_state == 2 and interface_change_track == False):
-			#RIGHT ENCODER
-			if (enc_right_delta != 0 and enc_right_seq == 2):
+			#LEFT ENCODER
+			if (enc_left_delta != 0 and enc_left_seq == 2):
 				#Starting to move means to switch track
+				playlist = subprocess.Popen("mpc playlist", shell=True, stdout=subprocess.PIPE)
+				playlist = playlist.communicate()
+				playlist = str(playlist)
+				playlist = playlist[2:]
+				playlist = playlist[:-7]
+				mopidy_playlist = playlist.split('\\n')
 				interface_change_track = True
 		
 		if (interface_change_track == True):
-			#RIGHT ENCODER
-			if (enc_right_delta != 0 and enc_right_seq == 2):
+			#LEFT ENCODER
+			if (enc_left_delta != 0 and enc_left_seq == 2):
 				menu_timeout = 0
-				if (enc_right_delta<0):
+				if (enc_left_delta<0):
 					if (menu_lcd_position < 4):
 						menu_lcd_position +=1
 					elif (menu_lcd_start < mopidy_playlist_length - 4):
 						menu_lcd_start += 1
 					if (menu_position < mopidy_playlist_length -1):
 						menu_position += 1
-				elif (enc_right_delta>0):
+				elif (enc_left_delta>0):
 					if (menu_lcd_position > 1):
 						menu_lcd_position -= 1
 					elif (menu_lcd_start > 0):
