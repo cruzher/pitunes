a8704ce0059ed36b2b172c002ec0c0a3e2a564b7
cruzher <kevin@kevins-mbp.lan>
2013-09-12 17:22:49 +0200

    .

---
+++ pot-test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import smbus
+from time import sleep
+
+bus = smbus.SMBus(1)
+addr = Ax2A
+
+def read():
+	wiper = bus.read_byte_data(address, 3)
+	return wiper
+
+def write(value):
+	bus.write_byte_data(address, 1, value)
+
+while True:
+	print read()
+
+	sleep(1)
