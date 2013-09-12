7a77c9f98b1eb0f7f9243e3b4f284fadc8dce814
cruzher <kevin@kevins-mbp.lan>
2013-09-12 17:26:27 +0200

    .

--- pot-test.py
+++ pot-test.py
@@ -7,11 +7,11 @@ bus = smbus.SMBus(1)
 addr = 0x2A
 
 def read():
-	wiper = bus.read_byte_data(address, 3)
+	wiper = bus.read_byte_data(addr, 3)
 	return wiper
 
 def write(value):
-	bus.write_byte_data(address, -1, value)
+	bus.write_byte_data(addr, -1, value)
 
 while True:
 	print read()
