7e32026361a828ac9ea1be4eb7e89b3f3a7c33fe
cruzher <kevin@Kevins-MBP.local>
2013-10-08 18:00:36 +0200

    .

---
+++ test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+import string
+from subprocess import Popen, PIPE
+
+current_song = Popen("mpc |head -2 |tail -1", shell=True, stdout=PIPE).stdout.read()
+current_song = current_song.split(" ")
