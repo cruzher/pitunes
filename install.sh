a52c72208301eb35596c335fa3c6e737adc6358f
cruzher <kevin@Kevins-MBP.local>
2013-10-18 13:21:59 +0200

    .

--- install.sh
+++ install.sh
@@ -9,59 +9,59 @@ sudo modprobe ipv6
 
 cd $HOME
 
-echo "(01/) ADDING APT SOURCES/KEYS"
+echo "(01/10) ADDING APT SOURCES/KEYS"
 wget -q -O - http://apt.mopidy.com/mopidy.gpg | sudo apt-key add -
 sudo wget -q -O /etc/apt/sources.list.d/mopidy.list http://apt.mopidy.com/mopidy.list
 
-echo "(02/) APT UPDATE / UPGRADE"
-sudo aptitude update
-sudo aptitude -y upgrade
+echo "(02/10) APT UPDATE / UPGRADE"
+sudo aptitude update > /dev/null
+sudo aptitude -y upgrade > /dev/null
 
-echo "(03/) INSTALLING DEPENDENCIES"
-sudo aptitude install -y build-essential python-smbus i2c-tools python-dev python-rpi.gpio python-setuptools mpc screen
+echo "(03/10) INSTALLING DEPENDENCIES"
+sudo aptitude install -y build-essential python-smbus i2c-tools python-dev python-rpi.gpio python-setuptools mpc screen > /dev/null
 
-echo "(04/) INSTALLING WIRINGPI2"
+echo "(04/10) INSTALLING WIRINGPI2"
 cd $HOME/pitunes/wiringPi/
 ./build
 cd ..
 
-echo "(05/) INSTALLING WIRINGPI2-PYTHON"
-git clone https://github.com/Gadgetoid/WiringPi2-Python.git
+echo "(05/10) INSTALLING WIRINGPI2-PYTHON"
+git clone https://github.com/Gadgetoid/WiringPi2-Python.git > /dev/null
 cd WiringPi2-Python/
-sudo python setup.py install
+sudo python setup.py install > /dev/null
 cd ..
 
-echo "(06/) INSTALLING PY-GAUGETTE"
-git clone git://github.com/guyc/py-gaugette.git
+echo "(06/10) INSTALLING PY-GAUGETTE"
+git clone git://github.com/guyc/py-gaugette.git > /dev/null
 cd py-gaugette
-git checkout wiringpi2
-sudo python setup.py install
+git checkout wiringpi2 > /dev/null
+sudo python setup.py install > /dev/null
 cd ..
 
-echo "(07/) INSTALLING MOPIDY"
-sudo apt-get install -y mopidy
+echo "(07/10) INSTALLING MOPIDY"
+sudo apt-get install -y mopidy > /dev/null
 mkdir $HOME/.config/mopidy
 cp $HOME/pitunes/conf/mopidy.conf $HOME/.config/mopidy/
-sudo sed -i "/exit 0/ c\ " /etc/rc.local
-echo "screen -dmS mopidy mopidy --config=/home/pi/.config/mopidy/mopidy.conf" | sudo tee -a /etc/rc.local
-echo "exit 0" | sudo tee -a /etc/rc.local
+sudo sed -i "/exit 0/ c\ " /etc/rc.local > /dev/null
+echo "screen -dmS mopidy mopidy --config=/home/pi/.config/mopidy/mopidy.conf" | sudo tee -a /etc/rc.local > /dev/null
+echo "exit 0" | sudo tee -a /etc/rc.local > /dev/null
 
-echo "(08/) INSTALLING SHAIRPORT"
+echo "(08/10) INSTALLING SHAIRPORT"
 wget -q http://files.pixor.se/install.scripts/rpi/shairport.sh
 chmod +x shairport.sh
-./shairport.sh piTunes
+./shairport.sh piTunes > /dev/null
 rm shairport.sh
 
-echo "(09/) INSTALLING WEBSERVICE"
-sudo apt-get install -y lighttpd
-sudo apt-get install -y mysql-server
-sudo apt-get install -y php5-common php5-cgi php5
-sudo apt-get install -y php5-mysql
-sudo apt-get install -y phpmyadmin
-sudo lighty-enable-mod fastcgi-php
-sudo service lighttpd force-reload
+echo "(09/10) INSTALLING WEBSERVICE"
+sudo apt-get install -y lighttpd > /dev/null
+sudo apt-get install -y mysql-server > /dev/null
+sudo apt-get install -y php5-common php5-cgi php5 > /dev/null
+sudo apt-get install -y php5-mysql > /dev/null
+sudo apt-get install -y phpmyadmin > /dev/null
+sudo lighty-enable-mod fastcgi-php > /dev/null
+sudo service lighttpd force-reload > /dev/null
 
-echo "(10/) COPY WEBGUI TO APACHE-ROOT"
+echo "(10/10) COPY WEBGUI TO APACHE-ROOT"
 sudo chmod 777 /var/www
 rm -rf /var/www/*
 cp -R pitunes/webgui/* /var/www/
