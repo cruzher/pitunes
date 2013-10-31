8e153f35d3b74ffcf6b71f5b4bac8ae54c397f6b
cruzher <kevin@Kevins-MBP.local>
2013-10-31 17:48:44 +0100

    .

--- webgui/test.php
+++ webgui/test.php
@@ -2,6 +2,6 @@
 	error_reporting(E_ALL);
 	ini_set('display_errors', '1');
 
-	exec('sudo update-rd.d shairport defaults');
-	exec('sudo service shairport start');
+	exec('sudo /sbin/iwlist wlan0 scan', $wifiscan);
+	echo $wifiscan;
 ?>
