62a5d7b239c9e3f35849efbade8eb7a9cfe69a77
cruzher <kevin@Kevins-MBP.local>
2013-11-19 18:39:22 +0100

    .

--- playlist.php
+++ playlist.php
@@ -2,7 +2,7 @@
 	mysql_connect("localhost", "pitunes", "pitunes");
 	mysql_select_db("pitunes");
 	
-	if (isset($_GET['spotify'])) {
+	if (isset(getopt('spotify:'))) {
 		$spotify = mysql_fetch_assoc(mysql_query("SELECT available_playlists FROM general"));
 		$playlists = explode(",", $spotify['available_playlists']);
 		foreach ($playlists as $playlist) {
