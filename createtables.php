eb3c6b44844c2c3b9f118c55b39b27129c0879d0
cruzher <kevin@Kevins-MBP.local>
2013-12-09 18:13:47 +0100

    .

--- createtables.php
+++ createtables.php
@@ -6,9 +6,13 @@
 	mysql_select_db("pitunes");
 
 	//radiostations
-	//$radiostations = "CREATE TABLE radiostations (id int AUTO_INCREMENT, name varchar(20), url varchar(200) PRIMARY KEY (id))";
+	$radiostations = "CREATE TABLE radiostations (";
+	$radiostations .= "id int AUTO_INCREMENT, ";
+	$radiostations .= "name varchar(20), ";
+	$radiostations .= "url varchar(200), ";
+	$radiostations .= "PRIMARY KEY (id))";
 
-	/*//settings
+	//settings
 	$settings = "CREATE TABLE settings (";
 	$settings .= "id int AUTO_INCREMENT, ";
 	$settings .= "airplay_status tinyint(4), ";
@@ -21,14 +25,14 @@
 	$settings .= "nameserver_two varchar(100), ";
 	$settings .= "spotify_status tintint(4), ";
 	$settings .= "spotify_user varchar(100), ";
-	$settings .= "spotify_pass varchar(100) ";
+	$settings .= "spotify_pass varchar(100), ";
 	$settings .= "PRIMARY KEY (id))";
 
 	//spotiy_playlists
 	$spotify = "CREATE TABLE spotify_playlists (";
 	$spotify .= "id int AUTO_INCREMENT, ";
 	$spotify .= "name varchar(200), ";
-	$spotify .= "alias varchar(20) ";
+	$spotify .= "alias varchar(20), ";
 	$spotify .= "PRIMARY KEY (id))";
 
 	//wifi_networks
@@ -40,15 +44,15 @@
 	$wifi_networks .= "dhcp tinyint(4), ";
 	$wifi_networks .= "ipaddress varchar(100), ";
 	$wifi_networks .= "netmask varchar(100), ";
-	$wifi_networks .= "gateway varchar(100) ";
-	$wifi_networks .= "PRIMARY KEY (id))";*/
+	$wifi_networks .= "gateway varchar(100), ";
+	$wifi_networks .= "PRIMARY KEY (id))";
 	
-	mysql_query("CREATE TABLE radiostations (id int AUTO_INCREMENT, name varchar(20), url varchar(200), PRIMARY KEY (id))");
-	//mysql_query($settings);
-	//mysql_query($spotify);
-	//mysql_query($wifi_networks);
+	mysql_query($radiostations);
+	mysql_query($settings);
+	mysql_query($spotify);
+	mysql_query($wifi_networks);
 	
-	/*$mekk = mysql_num_rows(mysql_query("SELECT * FROM settings"));
+	$mekk = mysql_num_rows(mysql_query("SELECT * FROM settings"));
 
 	if (!$mekk) {
 		$insert = "INSERT INTO settings(";
@@ -63,5 +67,5 @@
 		$insert .= "'8.8.4.4')";
 
 		mysql_query($insert);
-	}*/
+	}
 ?>
