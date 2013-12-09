1d4e521f21936ceaf00336296cfb47411efc4ddc
cruzher <kevin@Kevins-MBP.local>
2013-12-09 18:48:16 +0100

    .

--- test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-import string
-from subprocess import Popen, PIPE
-
-current_song = Popen("mpc |head -2 |tail -1", shell=True, stdout=PIPE).stdout.read()
-current_song = current_song.split(" ")
-print current_song
--- update.sh
+++ update.sh
@@ -2,6 +2,4 @@
 
 cd /home/pi/pitunes/
 git pull
-rm -rf /var/www/*
-cp -R webgui/* /var/www/
 
