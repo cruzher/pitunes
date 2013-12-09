c0424afb6083b7fe02124cfbf6b4cbc57e3f26cb
cruzher <kevin@Kevins-MBP.local>
2013-12-09 20:13:24 +0100

    .

--- webgui/pitunes.js
+++ webgui/pitunes.js
@@ -1,17 +1,19 @@
 
-$("#airplay").submit(function( event ) {
+$(document).ready(function(){
+	$("#airplay").submit(function( event ) {
 
-	event.preventDefault();
+		event.preventDefault();
 
-	var $form = $( this ),
-    airplay_status = $form.find( "input[name='airplay_status']" ).val(),
-    airplay_name = $form.find( "input[name='airplay_name']" ).val(),
-    url = $form.attr( "action" );
+		var $form = $( this ),
+	    airplay_status = $form.find( "input[name='airplay_status']" ).val(),
+	    airplay_name = $form.find( "input[name='airplay_name']" ).val(),
+	    url = $form.attr( "action" );
 
-    var posting = $.post( url, {"airplay": 1, "airplay_status": airplay_status, "airplay_name": airplay_name } );
+	    var posting = $.post( url, {"airplay": 1, "airplay_status": airplay_status, "airplay_name": airplay_name } );
 
-    posting.done(function( data ) {
-    	//Do stuff
-    	alert(data);
-  	});
+	    posting.done(function( data ) {
+	    	//Do stuff
+	    	alert(data);
+	  	});
+	});
 });
