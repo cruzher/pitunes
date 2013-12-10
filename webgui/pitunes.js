42f9f3fbe23f87f11740f46b39c1ca646b67e1a6
cruzher <kevin@Kevins-MBP.local>
2013-12-10 18:18:30 +0100

    .

--- webgui/pitunes.js
+++ webgui/pitunes.js
@@ -21,8 +21,12 @@ $(document).ready(function() {
 });
 
 function addPlaylist(playlist) {
-    $('body').append('<div id="box_background"></div>');
-    $('body').append('<div id="box"></div>');
+    if (playlist.length > 19 ) {
+        $('body').append('<div id="box_background"></div>');
+        $('body').append('<div id="box"></div>');
+        $('#box').append('<h1>Add playlist to LCD</h1>');
+        $('#box').append('<span class="center">The name of the playlist exceeds 19 characters and you need to enter an alias to show in the LCD</span>');
+    }
 }
 
 function removePlaylist(playlist) {
