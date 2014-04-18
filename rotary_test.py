b2768fa07075383968652a84ba3a2a7dd79443fe
cruzher <cruzher@Marvin.local>
2014-04-18 13:08:51 +0200

    test

---
+++ rotary_test.py
@@ -0,0 +1,26 @@
+import gaugette.rotary_encoder
+import gaugette.switch
+from time import sleep
+
+#Rotary Encoders
+enc_right_pin_a = 11
+enc_right_pin_b = 10
+sw_right_pin = 6
+enc_left_pin_a = 14
+enc_left_pin_b = 13
+sw_left_pin = 12
+enc_right = gaugette.rotary_encoder.RotaryEncoder(enc_right_pin_a, enc_right_pin_b)
+sw_right = gaugette.switch.Switch(sw_right_pin)
+enc_left = gaugette.rotary_encoder.RotaryEncoder(enc_left_pin_a, enc_left_pin_b)
+sw_left = gaugette.switch.Switch(sw_left_pin)
+
+while True:
+	enc_right_delta = enc_right.get_delta()
+	enc_right_seq = enc_right.rotation_sequence()
+	enc_left_delta = enc_left.get_delta()
+	enc_left_seq = enc_left.rotation_sequence()
+	sw_right_state = sw_right.get_state()
+	sw_left_state = sw_left.get_state()
+
+	print enc_right_delta
+	sleep(.01)
