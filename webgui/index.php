44aebf43754a883d4f3e26f6638c36333de19f4e
cruzher <cruzher@Marvin.local>
2014-12-07 22:26:47 +0100

    .

--- webgui/index.php
+++ webgui/index.php
@@ -13,8 +13,6 @@
 			<div id="menu">
 				<?php
 					if(isset($_GET['settings'])) { echo '<a href="?settings"><div class="menu_item" style="border-color: orange;">Settings</div></a>'; } else { echo '<a href="?settings"><div class="menu_item">Settings</div></a>'; }
-					if(isset($_GET['network'])) { echo '<a href="?network"><div class="menu_item" style="border-color: orange;">LAN</div></a>'; } else { echo '<a href="?network"><div class="menu_item">LAN</div></a>'; }
-					if(isset($_GET['wirelessnetwork'])) { echo '<a href="?wirelessnetwork"><div class="menu_item" style="border-color: orange;">Wireless LAN</div></a>'; } else { echo '<a href="?wirelessnetwork"><div class="menu_item">Wireless LAN</div></a>'; }
 					if(isset($_GET['radiostations'])) { echo '<a href="?radiostations"><div class="menu_item" style="border-color: orange;">Radiostations</div></a>'; } else { echo '<a href="?radiostations"><div class="menu_item">Radiostations</div></a>'; }
 					if(isset($_GET['spotify'])) { echo '<a href="?spotify"><div class="menu_item" style="border-color: orange;">Spotify</div></a>'; } else { echo '<a href="?spotify"><div class="menu_item">Spotify</div></a>'; }
 					if(isset($_GET['system'])) { echo '<a href="?system"><div class="menu_item" style="border-color: orange;">System</div></a>'; } else { echo '<a href="?system"><div class="menu_item">System</div></a>'; }
